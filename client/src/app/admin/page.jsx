b1ddf191b689f3627ad1ca44154cb1196a855c0f
kakashib2k <ishanprakashkulkarni@gmail.com>
2024-05-18 14:01:28 +0530

    added feature to create tree

--- client/src/app/admin/page.jsx
+++ client/src/app/admin/page.jsx
@@ -1,36 +1,34 @@
 "use client";
-
-import styles from "./admin.module.css"
-import {backendBaseURL} from "@/constants/index"
-import { useEffect, useState } from "react";
+// import {backendBaseURL} from "@/constants/index"
+// import { useEffect, useState } from "react";
 
 export default function Me() {
-  const [user, setUser] = useState(false);
-  useEffect(() => {
-    async function getUserInfo() {
-      try {
-        let res = await fetch(`${backendBaseURL}/user/userInfo`, {
-          method: "GET",
-          credentials: "include",
-          headers: {
-            Accept: "applications/json",
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Credentials": true,
-          },
-        });
-        let data = await res.json();
-        console.log(data.name);
-        setUser(data);
-        return data;
-      } catch (error) {
-        console.log('error getUserInfo', error);
-      }
-    }
-    getUserInfo();
-  }, []);
+  // const [user, setUser] = useState(false);
+  // useEffect(() => {
+  //   async function getUserInfo() {
+  //     try {
+  //       let res = await fetch(`${backendBaseURL}/user/userInfo`, {
+  //         method: "GET",
+  //         credentials: "include",
+  //         headers: {
+  //           Accept: "applications/json",
+  //           "Content-Type": "application/json",
+  //           "Access-Control-Allow-Credentials": true,
+  //         },
+  //       });
+  //       let data = await res.json();
+  //       console.log(data.name);
+  //       setUser(data);
+  //       return data;
+  //     } catch (error) {
+  //       console.log('error getUserInfo', error);
+  //     }
+  //   }
+  //   getUserInfo();
+  // }, []);
   return (
-    <main className={styles.container}>
-      <h3>Your Info</h3>
+    <main>
+      {/* <h3>Your Info</h3>
       <ul>
         <li>
             Your Name: {user.name}
@@ -41,7 +39,7 @@ export default function Me() {
         <li>
             Your Email: {user.email}
         </li>
-      </ul>
+      </ul> */}
     </main>
   );
 }
