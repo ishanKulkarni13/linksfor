22194a39526aa0d3bc2ccff268494a90e5c8f23e
kakashib2k <ishanprakashkulkarni@gmail.com>
2024-07-04 21:51:05 +0530

    fixed built time error beacause of React Hook useTreeUID is called conditionally

--- client/src/components/treePreview/treePreview.jsx
+++ client/src/components/treePreview/treePreview.jsx
@@ -3,11 +3,16 @@ import styles from "./treePreview.module.css"
 import { useTreeUID } from "@/hooks/treeUID"
 
 export default function TreePreview({refresh, treeUID, showBorder}) {
-  if(!treeUID)  treeUID = useTreeUID();
+  const  componentTreeUID = useTreeUID();
+
+  if(!treeUID) treeUID = componentTreeUID;
+  
   return (
     // <div className={styles.container} >
       <div className={styles.preview} >
-        <iframe className={`${styles.iframe} ${showBorder && styles.showBorder}`}   src={`/tree/${treeUID}`} title="tree Preview" scrolling="yes" ></iframe>
+       {
+        treeUID &&  <iframe className={`${styles.iframe} ${showBorder && styles.showBorder}`}   src={`/tree/${treeUID}`} title="tree Preview" scrolling="yes" ></iframe>
+       }
       </div>
     // </div>
   )
