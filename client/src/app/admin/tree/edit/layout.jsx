c3de2061682e6c01c8afdab8f9621fbcacced61e
kakashib2k <ishanprakashkulkarni@gmail.com>
2024-07-07 17:26:02 +0530

    fixed scrolling issue on smalldevices fir edit page

--- client/src/app/admin/tree/edit/layout.jsx
+++ client/src/app/admin/tree/edit/layout.jsx
@@ -1,15 +1,18 @@
 import TreePreview from "@/components/treePreview/treePreview";
 import styles from "./style.module.css";
+import TreePreviewToggleButton from "@/components/treePreview/treePreviewToggleButton/treePreviewToggleButton";
 export default function AdminTreeEditRootLayout({ children }) {
   return (
+    <>
     <div className={styles.rootContainer}>
       <div className={styles.childrenContainer}>{children}</div>
       <div className={styles.treePreviewContainer}>
         <div className={styles.treePreview}>
-          
           <TreePreview showBorder={true} />
         </div>
       </div>
     </div>
+    <TreePreviewToggleButton/>
+    </>
   );
 }
