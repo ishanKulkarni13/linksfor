0567ddd18879586cd130aae32ed060d0f69cb4c5
kakashib2k <ishanprakashkulkarni@gmail.com>
2024-07-25 22:29:33 +0530

    improved dark/light mode theme toggle - added animations when switching to different mode using view transistion and changed theme toggle

--- client/src/components/buttons/theme/changeThemeButton.jsx
+++ client/src/components/buttons/theme/changeThemeButton.jsx
@@ -1,42 +1,97 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { useTheme } from "next-themes"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons"
+import * as React from "react";
+import { useTheme } from "next-themes";
+import { useState, useEffect, useRef } from "react";
+import { flushSync } from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import styles from "./style.module.css";
 
 export function ThemeModeToggle() {
-  const { setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme } = useTheme();
+  let isDarkThemeActive = resolvedTheme == "dark" ? true : false;
+  const ref = useRef(null);
+
+  const toggleDarkMode = async () => {
+    if (
+      !ref.current ||
+      !document.startViewTransition ||
+      window.matchMedia("(prefers-reduced-motion: reduce)").matches
+    ) {
+      if (resolvedTheme == "light") {
+        setTheme("dark");
+      } else {
+        setTheme("light");
+      }
+      return;
+    }
+
+    await document.startViewTransition(() => {
+      flushSync(() => {
+        if (resolvedTheme == "light") {
+          setTheme("dark");
+        } else {
+          setTheme("light");
+        }
+      });
+    }).ready;
+
+    const { top, left, width, height } = ref.current.getBoundingClientRect();
+    const x = left + width / 2;
+    const y = top + height / 2;
+    const right = window.innerWidth - left;
+    const bottom = window.innerHeight - top;
+    const maxRadius = Math.hypot(Math.max(left, right), Math.max(top, bottom)) + 50;
+
+    document.documentElement.animate(
+      {
+        clipPath: [
+          `circle(${width}px at ${x}px ${y}px)`,
+          `circle(${maxRadius}px at ${x}px ${y}px)`,
+        ],
+      },
+      {
+        duration: 750,
+        easing: "ease-in-out",
+        pseudoElement: "::view-transition-new(root)",
+      }
+    );
+  };
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <button className={styles.themeButton}>
-            <FontAwesomeIcon  icon={faSun} className="h-[1.3rem] w-[1.3rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"  />
-            <FontAwesomeIcon icon={faMoon}  className="absolute h-[1.3rem] w-[1.3rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
-          <span className="sr-only">Toggle theme</span>
+    <>
+      {/* <button ref={ref} onClick={toggleDarkMode} >
+    {resolvedTheme}
+   </button> */}
+        <button
+          ref={ref}
+          onClick={toggleDarkMode}
+          className={`${
+            isDarkThemeActive ? styles.dark  : styles.light
+          } ${styles.themeToggleButton}`}
+          aria-label="Toggle Theme"
+        >
+          <span
+            className={`${styles.shape} ${isDarkThemeActive ? styles.moon : styles.sun}`}
+          ></span>
+          <span className={`${styles.raysContainer}`}>
+          <span className={`${styles.ray}`}></span>
+            <span className={`${styles.ray}`}></span>
+            <span className={`${styles.ray}`}></span>
+            <span className={`${styles.ray}`}></span>
+          </span>
         </button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+        {/* {`${isDarkThemeActive}`} */}
+    </>
+  );
 }
