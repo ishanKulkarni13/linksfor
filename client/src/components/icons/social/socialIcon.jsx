68790debdc8ff0d5c4523fd177fe17ddd9e86cdb
kakashib2k <ishanprakashkulkarni@gmail.com>
2025-04-24 00:05:58 +0530

    fixing social icons

--- client/src/components/icons/social/socialIcon.jsx
+++ client/src/components/icons/social/socialIcon.jsx
@@ -1,35 +1,39 @@
 // import { socialIcons } from "@/constants/tree";
 // import React from "react";
 // import { HiQuestionMarkCircle } from "react-icons/hi";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-// export function SocialIcon({ iconName , ...props}) {
+// export function SocialIcon({ iconName, ...props }) {
+//   const IconComponent = socialIcons[iconName.toLowerCase()] 
+//     ? FontAwesomeIcon 
+//     : HiQuestionMarkCircle;
+
+//   const iconProps = socialIcons[iconName.toLowerCase()] 
+//     ? { icon: socialIcons[iconName.toLowerCase()].icon } 
+//     : {};
 
 //   return (
-//     <>
-//       {socialIcons[iconName.toLowerCase()] ? (
-//         React.createElement(socialIcons[iconName.toLowerCase()].icon, props)
-//       ) : (
-//         <HiQuestionMarkCircle {...props} />
-//       )}
-//     </>
+//     <IconComponent {...props} {...iconProps} />
 //   );
-//   }
+// }
+
+// -------------------------------------
 
 import { socialIcons } from "@/constants/tree";
 import React from "react";
 import { HiQuestionMarkCircle } from "react-icons/hi";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export function SocialIcon({ iconName, ...props }) {
-  const IconComponent = socialIcons[iconName.toLowerCase()] 
-    ? FontAwesomeIcon 
-    : HiQuestionMarkCircle;
+export function SocialIcon({ iconName = "", ...props }) {
+  const key = iconName.toLowerCase();
+  const match = socialIcons[key];
 
-  const iconProps = socialIcons[iconName.toLowerCase()] 
-    ? { icon: socialIcons[iconName.toLowerCase()].icon } 
-    : {};
+  // Choose component and prop‑shape
+  const Component = match ? FontAwesomeIcon : HiQuestionMarkCircle;
+  const extra    = match ? { icon: match.icon } : {};
 
-  return (
-    <IconComponent {...props} {...iconProps} />
-  );
+  return <Component {...extra} {...props} />;
 }
+
+// --------------------------------
+
