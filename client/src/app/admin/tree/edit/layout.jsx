899df394ec147245574eaf8e79e32e91a5a87c6f
kakashib2k <ishanprakashkulkarni@gmail.com>
2025-05-26 00:33:11 +0530

    trying to add a preview on bigscreent in tree edit pages

--- client/src/app/admin/tree/edit/layout.jsx
+++ client/src/app/admin/tree/edit/layout.jsx
@@ -1,18 +1,41 @@
 import TreePreview from "@/components/treePreview/treePreview";
 import styles from "./style.module.css";
 import TreePreviewToggleButton from "@/components/treePreview/treePreviewToggleButton/treePreviewToggleButton";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 export default function AdminTreeEditRootLayout({ children }) {
+  
+  // return(
+
+
+  //     <ResizablePanelGroup
+  //     direction="horizontal" className={styles.rootContainer}
+  //   >
+
+  //     <ResizablePanel defaultSize={25}>
+  //        <div className={styles.childrenContainer}>{children}</div>
+  //     </ResizablePanel>
+
+  //     <ResizableHandle withHandle />
+
+  //     <ResizablePanel defaultSize={75}>
+  //        <div className={styles.treePreviewContainer}>
+  //         <TreePreview className={styles.treePreview} showBorder={true} />
+  //       </div>
+  //     </ResizablePanel>
+
+  //   </ResizablePanelGroup>
+  // ) 
+  
   return (
     <>
-    <div className={styles.rootContainer}>
-      <div className={styles.childrenContainer}>{children}</div>
-      {/* <div className={styles.treePreviewContainer}>
-        <div className={styles.treePreview}>
-          <TreePreview showBorder={true} />
-        </div>
-      </div> */}
-    </div>
-    <TreePreviewToggleButton alwaysVisible />
+      <div className={styles.rootContainer}>
+        <div className={styles.childrenContainer}>{children}</div>
+ 
+        {/* <div className={styles.treePreviewContainer}>
+          <TreePreview className={styles.treePreview} showBorder={true} />
+        </div> */}
+      </div>
+      <TreePreviewToggleButton alwaysVisible />
     </>
   );
 }
--- client/src/components/treePreview/treePreview.jsx
+++ client/src/components/treePreview/treePreview.jsx
@@ -3,14 +3,13 @@ import PortalIframe from "@/components/portalIFrame/iframe.jsx";
 import styles from "./treePreview.module.css";
 import { useTreeUID } from "@/hooks/treeUID";
 
-export default function TreePreview({ refresh, treeUID, showBorder }) {
+export default function TreePreview({ refresh, treeUID, showBorder , className}) {
   const componentTreeUID = useTreeUID();
 
   if (!treeUID) treeUID = componentTreeUID;
 
   return (
-    // <div className={styles.container} >
-    <div className={styles.preview}>
+    <div className={`${styles.preview} ${className}`}>
       {treeUID && (
         <PortalIframe
           className={`${styles.iframe} ${showBorder && styles.showBorder}`}
@@ -22,6 +21,5 @@ export default function TreePreview({ refresh, treeUID, showBorder }) {
       )}
       
     </div>
-    // </div>
   );
 }
