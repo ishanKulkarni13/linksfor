e0901fa007235317fa6190d2261af2cee4488646
KakashiB2k <ishanprakashkulkarni@gmail.com>
2025-08-21 20:36:27 +0530

    updated air white theme with new approach

--- client/src/components/themes/preDefined/airWhite/background/background.jsx
+++
@@ -1,6 +0,0 @@
-import styles from './background.module.css'
-export default function Background() {
-  return (
-    <div className={styles.background}></div>
-  )
-}
--- client/src/components/themes/preDefined/classic/theme.jsx
+++ client/src/components/themes/preDefined/classic/theme.jsx
@@ -1,55 +1,7 @@
-// import Socials from "@/components/themes/themeComponents/socials/default/socials.jsx";
-// import Background from "./background/background";
-// import Links from "./links/links";
-// import Pofile from "./profile/profile";
-import styles from "./theme.module.css";
 
-// import all from @/components/themes/themeComponents
-import Socials from "@/components/themes/themeComponents/socials/default/socials.jsx";
-import Background from "@/components/themes/themeComponents/background/background";
-import Links from "@/components/themes/themeComponents/links/links";
-import Pofile from "@/components/themes/themeComponents/profile/profile";
+import styles from "./theme.module.css";
+import Tree from "@/components/themes/themeComponents/tree/theme";
 
 export default function TreeTheme({ tree }) {
-  return (
-    <>
-    <Background />
-      <main className={` ${styles.treeContainer}`}>
-
-        <div className={styles.profileContainer}>
-          <Pofile
-            treeName={tree.treeName}
-            treePicture={tree.treePicture}
-            treeBio={tree.treeBio}
-          />
-        </div>
-
-        <div
-          className={styles.treeContentContainer}
-          style={{
-            flexDirection: 
-              // tree?.theme?.themePreference?.socialIcons?.socialIconsPlacement &&
-              tree?.theme?.themePreference?.socialIcons?.socialIconsPlacement ==
-                "top"
-                ? `column`
-                : `column-reverse`,
-              // background: 
-              // tree?.theme?.themePreference?.socialIcons?.socialIconsPlacement &&
-              // tree?.theme?.themePreference?.socialIcons?.socialIconsPlacement ==
-              //   "top"
-              //   ? `yellow`
-              //   : `red`,
-          }}
-        >
-          <div className={styles.socialsContainer}>
-            <Socials socials={tree.treeContent.socials} />
-          </div>
-
-          <div className={styles.linksContainer}>
-            <Links links={tree.treeContent.links} />
-          </div>
-        </div>
-      </main>
-    </>
-  );
+  return <Tree tree={tree} styles={styles} />
 }
