d313e0d3b32f0c69531d805698b35cf4231d5013
KakashiB2k <ishanprakashkulkarni@gmail.com>
2025-08-22 20:58:30 +0530

    fixed - on mobile the x-margin of sand theme links was appearing to be zero and the border radius of the thumbnail was looking wired

--- client/src/components/themes/preDefined/sand/theme.jsx
+++ client/src/components/themes/preDefined/sand/theme.jsx
@@ -1,14 +1,4 @@
-// import Socials from "@/components/themes/themeComponents/socials/default/socials.jsx";
-// import Background from "./background/background";
-// import Links from "./links/links";
-// import Pofile from "./profile/profile";
 import styles from "./theme.module.css";
-
-// import all from @/components/themes/themeComponents
-import Socials from "@/components/themes/themeComponents/socials/default/socials.jsx";
-import Background from "@/components/themes/themeComponents/background/background";
-import Links from "@/components/themes/themeComponents/links/links";
-import Pofile from "@/components/themes/themeComponents/profile/profile";
 import Tree from "@/components/themes/themeComponents/tree/theme";
 
 export default function TreeTheme({ tree }) {
