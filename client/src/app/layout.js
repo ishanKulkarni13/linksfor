6771f029c2b2d45580483564ae1346faac624be9
kakashiB2k <ishanprakashkulkarni@gmail.com>
2025-08-26 11:05:36 +0530

    Added vercel Web Analytics

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -30,7 +30,8 @@ export default function RootLayout({ children }) {
           </>
          
         </ThemeProvider>
-        <Analytics />
+        <Analytics /> {/* For Vercel Web Analytics tool */}
+        
       </body>
     </html>
   );
